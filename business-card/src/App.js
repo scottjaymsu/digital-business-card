ac3435b4e23daee4c8ce1305b499d7a058b40917
Jay <scottjay@msu.edu>
2023-01-05 01:49:28 -0500

    completed Interests component with styling

--- business-card/src/App.js
+++ business-card/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Info from './components/Info.js' 
 import About from './components/About.js'
+import Interests from './components/Interests.js'
 
 function App() {
   return (
     <>
       <Info />
       <About />
+      <Interests />
     </>
   );
 }
